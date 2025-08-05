fa685875b2e0ff47091b1ec05ad2c9e2fd2287c1
junijaei <hjhj7895598@gmail.com>
2025-08-06 00:40:49 +0900

    feat: 기술 섹션 추가

--- component/section/Skills.tsx
+++ component/section/Skills.tsx
@@ -1,3 +1,24 @@
-export default function Skills() {
-  return <div></div>;
+import { Skill, SkillKey } from '@/types';
+import PointText from '@/component/PointText';
+
+export default function Skills({ skills }: { skills: Skill }) {
+  return (
+    <div className={'mx-12 flex gap-8'}>
+      {Object.keys(skills).map(key => (
+        <ul
+          key={key}
+          className={'w-full'}
+        >
+          <h3 className={'text-2xl font-bold'}>
+            {key}
+            <PointText>.</PointText>
+          </h3>
+          <hr className={'text-surface-300 my-2'} />
+          {skills[key as SkillKey].map(skill => (
+            <li key={skill}>· {skill}</li>
+          ))}
+        </ul>
+      ))}
+    </div>
+  );
 }
